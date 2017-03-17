512bdd4983f5fd75453a1cbd9d2e290887ec6123
ALUMNO-G <ALUMNO-G@UCSP.AULAB>
2017-03-17 12:31:22 -0500

    Hola prros

--- hiWorld.cpp/main.cpp
+++ hiWorld.cpp/main.cpp
@@ -5,5 +5,6 @@ using namespace std;
 int main()
 {
     cout << "Hola perros!" << endl;
+    cout << "Patricio, si yo xd, estoy muy emocionado por aprender a programar en general" << endl;
     return 0;
 }
